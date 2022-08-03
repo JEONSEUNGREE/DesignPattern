02488b6af256e40bc1fd061f05786fc31271fff1
JEONSEUNGREE <jws08219@naver.com>
2022-08-03 23:20:19 +0900

    chainOfResponsibility

---
+++ Java/src/chainOfResposibility/ChainOfResponsibilityMain.java
@@ -0,0 +1,17 @@
+package chainOfResposibility;
+
+
+// 특정 객체가 동작을 실패할 경우 다음 객체가 실행됨
+public class ChainOfResponsibilityMain {
+    public static void main(String[] args) {
+        Calculator plus =  new PlusCalculator();
+        Calculator sub =  new SubPlusCalculator();
+
+        plus.setNextCalculator(sub);
+        Request request1 = new Request(1, 2, "+");
+        Request request2 = new Request(1, 2, "-");
+        plus.process(request1);
+        plus.process(request2);
+
+    }
+}
